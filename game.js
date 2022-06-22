5f61066009924ce0aea670214c63855fc7c4e1f8
Henry Khosasih <henrylaurentkhosasih@gmail.com>
2022-06-22 12:42:53 +1000

    add animation and sound for clickec buttons

--- game.js
+++ game.js
@@ -1,6 +1,16 @@
-const gamePattern = [];
-
 const buttonColours = ["red", "blue", "green", "yellow"];
+
+const gamePattern = [];
+const userClickedPattern = [];
+
+$(document).on("keydown", nextSequence);
+
+$(".btn").on("click", function() {
+    let userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+})
 
 function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
@@ -8,9 +18,17 @@ function nextSequence() {
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}
 
-    let sound = new Audio("sounds/" + randomChosenColour + ".mp3");
+function playSound(name) {
+    let sound = new Audio("sounds/" + name + ".mp3");
     sound.play();
 }
 
-$(document).on("keydown", nextSequence);
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function() {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
